72cec689eafa915f85d6b6b2c07fdbc0d60992a4
vaibhavty <vaibhavtyagi@vaibhavs-MacBook-Air.local>
2014-09-06 00:36:42 -0400

    Example 1
    commit 2

--- set00/1.rkt
+++ set00/1.rkt
@@ -1,1 +1,6 @@
-(* 366 24 60 60)
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |1|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+(* 360 24 60 60)
+(* 36 10 24 60 60)
+(* 6 6 10 24 60 60) 
